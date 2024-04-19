98f8ba08d9c46f262c3a629ec8a9ded839808397
fsologureng <fsologur@uchile.cl>
2024-04-19 11:41:30 -0400

    Workflow: centralize external workflow

--- .github/test.sh
+++ .github/test.sh
@@ -1,15 +1,13 @@
 #!/bin/dash
 
-pip install -e /openedx/requirements/pdfXBlock
+pip install -e /openedx/requirements/app
 
-cd /openedx/requirements/pdfXBlock
+cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/pdfXBlock
+cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest pdf/tests.py
-
-rm -rf test_root
